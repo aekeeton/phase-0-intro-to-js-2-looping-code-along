ca658186211201fa1e8fce62d981f6a03892e48f
aekeeton <ashley.keeton@outlook.com>
2022-06-02 19:31:55 -0500

    commit tree 32f95ebb6bbf6473a139e8a2959ecad7055d331c

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    const cards = [];
+    for (let i = 0; i < 3; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(n) {
+    while (n > -1) {
+        console.log(n)
+        n--;
+    }
+}
